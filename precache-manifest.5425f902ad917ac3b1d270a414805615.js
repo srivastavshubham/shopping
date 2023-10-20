3f5f1233d08b98cc680ced2713ce3f013c6a075b
shubhan srivastava <shubhamda111@gmail.com>
2023-10-20 16:04:03 +0530

    Updates

---
+++ precache-manifest.5425f902ad917ac3b1d270a414805615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc842226ef30aa341e16d2aa2c0bd949",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "fa286d62b20be069af03",
+    "url": "/shopping/static/css/main.260a14e2.chunk.css"
+  },
+  {
+    "revision": "e855a2fb993132a5c322",
+    "url": "/shopping/static/js/2.16850c4f.chunk.js"
+  },
+  {
+    "revision": "f2f17439b958bf06f2a279ecb3d26c68",
+    "url": "/shopping/static/js/2.16850c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa286d62b20be069af03",
+    "url": "/shopping/static/js/main.4898b371.chunk.js"
+  },
+  {
+    "revision": "438f010c7b0342b5d4eb",
+    "url": "/shopping/static/js/runtime-main.d5c916ff.js"
+  }
+]);
